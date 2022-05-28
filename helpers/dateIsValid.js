c0933f3d7f062368aaadc7fae595ac67b0e50647
Fawzi E. Abdulfattah <iifawzie@gmail.com>
2022-05-28 22:32:26 +0200

    Update the isDateValid functionality
    Signed-off-by: Fawzi E. Abdulfattah <iifawzie@gmail.com>

--- helpers/dateIsValid.js
+++ helpers/dateIsValid.js
@@ -1,14 +1,12 @@
 export default function dateIsValid(input) {
-    if (input) {
-        const date = new Date(input);
-        if (typeof date === 'object' && date !== null && typeof date.getTime === 'function' &&
-            !isNaN(date) && date.getTime() > 0 && !(date.getTime() > new Date().getTime())
-        ) {
-            return true;
-        }
-        return false;
-    } else {
-        return true // if date is not empty, it's ok
+    if (!input) {
+        return true; // if input is empty, it's ok (no filter);
     }
 
+    const date = new Date(input);
+    if (typeof date === 'object' && date !== null && typeof date.getTime === 'function' &&
+        !isNaN(date) && date.getTime() > 0 && !(date.getTime() > new Date().getTime())
+    ) {
+        return true;
+    }
 }
